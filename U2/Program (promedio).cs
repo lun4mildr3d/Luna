6d2941815ce776a2b7229472c5ad600fb14c7b39
lun4mildr3d <146401129+lun4mildr3d@users.noreply.github.com>
2023-09-29 16:09:46 -0500

    promed

---
+++ U2/Program (promedio).cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p_3
+{
+    internal class Promedio
+    {
+        static void Main(string[] args)
+        {
+            int c1, c2, c3, c4, c5, c6;
+            float promedio;
+            Console.Write("Calificacion 1: ");
+            c1 = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Calificacion 2: ");
+            c2 = Convert.ToInt32 (Console.ReadLine() );
+            Console.Write("Calificacion 3: ");
+            c3 = Convert.ToInt32 (Console.ReadLine() );
+            Console.Write("Calificacion 4: ");
+            c4 = Convert.ToInt32 (Console.ReadLine() );
+            Console.Write("Calificacion 5: ");
+            c5 = Convert.ToInt32 (Console.ReadLine() );
+            Console.Write("Calificacion 6: ");
+            c6 = Convert.ToInt32 (Console.ReadLine() );
+            //PASO- CALCULAR PROMEDIO
+            promedio = (c1 + c2 + c3 + c4 + c5 + c6) / 6;
+            //PSO- IMPRIME EL RESULTADO
+            Console.WriteLine("Tu promedio es: " + promedio);
+        }
+    }
+}
