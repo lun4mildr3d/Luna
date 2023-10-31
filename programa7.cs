ccb99db000ef6f67b0f2f8245ae21c56a1e12e95
lun4mildr3d <146401129+lun4mildr3d@users.noreply.github.com>
2023-10-30 23:19:21 -0600

    Add files via upload

---
+++ programa7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace programa7 // Note: actual namespace depends on the project name.
+{
+    internal class Temperatura
+    {
+        static void Main(string[] args)
+        {
+            double T;
+            Console.WriteLine("¿Qué temperatura hay hoy?");
+            T = double.Parse(Console.ReadLine());
+
+            if (T >= 27)
+            {
+                Console.WriteLine("¡Hace calor!");
+            }
+            else if (T < 27 && T >= 20)
+            {
+                Console.WriteLine("¡Clima agradable!");
+            }
+            else
+            {
+                Console.WriteLine("¡Clima fresco");
+
+            }
+        }
+    }
+}
